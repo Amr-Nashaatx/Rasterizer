3fa22b4c86956793184118bce0bb99b61f43abf2
Amr Nashaat <amrnashaat8@gmaol.com>
2026-01-17 19:43:45 +0200

    temp

--- src/math/functions.ts
+++ src/math/functions.ts
@@ -1,6 +1,7 @@
 import { VIEWPORT_DISTANCE } from "../constants";
 import type { Plane } from "./Plane";
 import { Point } from "./point";
+import type { Triangle } from "./Triangle";
 import { Vector } from "./vector";
 
 export function interpolate(i0: number, d0: number, i1: number, d1: number) {
@@ -38,11 +39,36 @@ export function projectPoint(point: Point) {
   return projPoint;
 }
 
-export function signedDistance(p: Plane, vertex: Point) {
+export function computeSignedDistance(p: Plane, vertex: Point) {
   const normal = p.normal;
   const posVector = new Vector(vertex.x, vertex.y, vertex.z);
 
   return posVector.dot(normal) + p.D;
 }
 
-// export function clipTriangle
+export function clipTriangle(tri: Triangle, plane: Plane) {
+  const signedDistances = [];
+  signedDistances.push(computeSignedDistance(plane, tri.v0));
+  signedDistances.push(computeSignedDistance(plane, tri.v1));
+  signedDistances.push(computeSignedDistance(plane, tri.v2));
+
+  const allNegative =
+    signedDistances[0] < 0 && signedDistances[1] < 0 && signedDistances[2] < 0;
+  const allPositive =
+    signedDistances[0] > 0 && signedDistances[1] > 0 && signedDistances[2] > 0;
+
+  let posCount = 0;
+  let negCount = 0;
+  for (let d of signedDistances) {
+    if (d > 0) posCount++;
+    if (d < 0) negCount++;
+  }
+  const onlyOneIsPositive = posCount === 1 ? true : false;
+  const onlyOneIsNegative = negCount === 1 ? true : false;
+
+  if (allPositive) return [tri];
+  else if (allNegative) return [];
+  else if (onlyOneIsPositive) {
+  } else if (onlyOneIsNegative) {
+  }
+}
