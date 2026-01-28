4ffe7a12297998f5b42e6295cb00a964eb27e79f
DESKTOP-L4FFAHV\amrnash <amrnashaat8@gmail.com>
2026-01-28 19:23:27 +0200

    refactor interpolation by adding linearInterpolate function

--- src/TriangleDrawer.ts
+++ src/TriangleDrawer.ts
@@ -1,9 +1,8 @@
 import { Canvas } from "./canvas";
 import { LineDrawer } from "./LineDrawer";
-import type { Color } from "./math/color";
-import { interpolate, swapPoints } from "./math/functions";
+import { interpolate, linearInterpolate, sortByY } from "./math/functions";
 import { Point } from "./math/point";
-import type { Triangle } from "./math/Triangle";
+import { Triangle } from "./math/Triangle";
 
 export class TriangleDrawer {
   private canvas: Canvas;
@@ -20,83 +19,64 @@ export class TriangleDrawer {
   }
 
   drawFilledTriangle(t: Triangle, enableShading: boolean) {
-    let s1 = this.canvas.viewportToCanvas(t.v1);
     let s0 = this.canvas.viewportToCanvas(t.v0);
+    let s1 = this.canvas.viewportToCanvas(t.v1);
     let s2 = this.canvas.viewportToCanvas(t.v2);
 
-    let p0 = new Point(s0.x, s0.y, s0.z);
-    let p1 = new Point(s1.x, s1.y, s1.z);
-    let p2 = new Point(s2.x, s2.y, s2.z);
+    // Sort points by Y to match what linearInterpolate does internally
+    [s0, s1, s2] = sortByY(s0, s1, s2);
 
-    if (enableShading) {
-      p0.h = s0.h;
-      p1.h = s1.h;
-      p2.h = s2.h;
-    }
+    // Round Y values to integers for proper loop iteration and array indexing
+    s0.y = Math.round(s0.y);
+    s1.y = Math.round(s1.y);
+    s2.y = Math.round(s2.y);
 
-    // order the points according to y value ascending
-    if (p1.y < p0.y) swapPoints(p0, p1);
-    if (p2.y < p0.y) swapPoints(p0, p2);
-    if (p2.y < p1.y) swapPoints(p1, p2);
+    let { right: xRight, left: xLeft } = linearInterpolate(s0, s1, s2, "x");
 
-    // compute the all x vlaues of the tall side,
-    // and the short sides for each y value.
-    // note: y is the independent variable here
-    const x01 = interpolate(p0.y, p0.x, p1.y, p1.x);
-    const h01 = interpolate(p0.y, p0.h, p1.y, p1.h);
-
-    const x12 = interpolate(p1.y, p1.x, p2.y, p2.x);
-    const h12 = interpolate(p1.y, p1.h, p2.y, p2.h);
-
-    const x02 = interpolate(p0.y, p0.x, p2.y, p2.x);
-    const h02 = interpolate(p0.y, p0.h, p2.y, p2.h);
-
-    x01.pop();
-    const x012 = [...x01, ...x12];
-
-    h01.pop();
-    const h012 = [...h01, ...h12];
-
-    // determine which is left and which is right
-    const middle = p1.y - p0.y;
-    let xLeft: number[] = [];
     let hLeft: number[] = [];
-
-    let xRight: number[] = [];
     let hRight: number[] = [];
-
-    if (x02[middle] < x012[middle]) {
-      xLeft = x02;
-      hLeft = h02;
-
-      xRight = x012;
-      hRight = h012;
-    } else {
-      xLeft = x012;
-      hLeft = h012;
-
-      xRight = x02;
-      hRight = h02;
+    if (enableShading) {
+      let { right, left } = linearInterpolate(s0, s1, s2, "h");
+      hLeft = left;
+      hRight = right;
     }
-    // draw the horizontal sigments
-    for (let y = p0.y; y < p2.y; y++) {
-      const i = y - p0.y;
-      const xl = xLeft[i];
-      const hl = hLeft[i];
 
+    const { left: invZLeft, right: invZRight } = linearInterpolate(
+      s0,
+      s1,
+      s2,
+      "invZ",
+    );
+
+    for (let y = s0.y; y <= s2.y; y++) {
+      const i = y - s0.y;
+      const xl = xLeft[i];
       const xr = xRight[i];
-      const hr = hRight[i];
+      const izR = invZRight[i];
+      const izL = invZLeft[i];
 
       if (xl === undefined || xr === undefined) continue;
       const xStart = Math.ceil(xl);
       const xEnd = Math.floor(xr);
 
-      const hValuesOnX = interpolate(xStart, hl, xEnd, hr);
+      let hr: number,
+        hl: number,
+        hValuesOnX: number[] = [];
+      if (enableShading) {
+        hr = hRight[i];
+        hl = hLeft[i];
+        hValuesOnX = interpolate(xStart, hl, xEnd, hr);
+      }
+      const izValuesOnX = interpolate(xStart, izL, xEnd, izR);
       for (let x = xStart; x < xEnd; x++) {
         const pixel = new Point(x, y);
-        const h = hValuesOnX[x - xStart];
-        const shadedColor = t.color.scale(h);
-        this.canvas.putPixel(pixel, shadedColor);
+        const invZ = izValuesOnX[x - xStart];
+        pixel.invZ = invZ;
+        let finalColor = t.color;
+        if (enableShading) {
+          finalColor = t.color.scale(hValuesOnX[x - xStart]);
+        }
+        this.canvas.putPixel(pixel, finalColor);
       }
       this.canvas.updateCanvas();
     }
